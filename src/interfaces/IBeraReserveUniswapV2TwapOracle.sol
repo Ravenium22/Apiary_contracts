ca7ea0d4b1f485ee8ab73884b606dedef8520aab
Ravenium22 <efeberkakgul@gmail.com>
2025-12-21 02:49:18 +0300

    Cleanup

--- src/interfaces/IBeraReserveUniswapV2TwapOracle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.26;
-
-interface IBeraReserveUniswapV2TwapOracle {
-    function update() external;
-
-    function consult(uint256 amountIn) external returns (uint256 amountOut);
-}
